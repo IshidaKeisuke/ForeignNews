45f9661c4c7654ea80adef66c4c856f4a68790f0
IshidaKeisuke <k.ishida@algorithms.co.jp>
2022-10-09 14:45:35 +0900

    Next.js実装

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,37 +1,45 @@
 import type { NextPage } from 'next'
+import { useState } from "react";
+
 import Link from 'next/link'
 
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Todo } from '../types'
+import TodoForm from '../components/todos/TodoForm'
 
 interface Props {
   todos: Todo[]
 }
 
 const Home: NextPage<Props> = ({todos}) => {
+
   return (
-    <div>
-      <h2>
-        Todo一覧
-      </h2>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-              <Link 
-                href={{
-                  pathname: '/todos/[id]',
-                  query: {id: todo.id},
-                }}
-              >
-                <p>{todo.id}</p>
-              </Link>
-                <p>{todo.todo}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div>
+        <h2>
+          Todo一覧
+          <TodoForm />
+        </h2>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={todo.id}>
+                <Link 
+                  href={{
+                    pathname: '/todos/[id]',
+                    query: {id: todo.id},
+                  }}
+                >
+                  <p>{todo.id}</p>
+                </Link>
+                  <p>{todo.todo}</p>
+
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
 
--- frontend/pages/todos/[id].tsx
+++ frontend/pages/todos/[id].tsx
@@ -1,6 +1,4 @@
 import Link from 'next/link'
-
-// todo：getStaticPropsから取得したデータ
 export default ({ todo }) => {
   return (
     <div>
@@ -14,25 +12,18 @@ export default ({ todo }) => {
 }
 
 export const getStaticPaths = async () => {
-  // 外部APIエンドポイントを呼び出しデータ取得
   const res = await fetch("http://localhost:3000/todos")
   const todos = await res.json()  
 
-  // 事前ビルドしたいパスを指定
-  const paths = todos.map((todo) => ({
+  const paths = todos.map((todo: any) => ({
     params: {
-      // ファイル名と合わせる ※文字列指定
       id: todo.id.toString(),
     },
   }))
-  // paths：事前ビルドするパス対象を指定するパラメータ
-  // fallback：事前ビルドしたパス以外にアクセスしたときのパラメータ true:カスタム404Pageを表示 false:404pageを表示
   return { paths, fallback: false }
 }
 
-// paramsには上記pathsで指定した値が入る（1todoずつ）
-export const getStaticProps = async ({ params }) => {  
-  // 外部APIエンドポイントを呼び出しデータ取得
+export const getStaticProps = async ({params}: {params: any}) => {  
   const res = await fetch(`http://localhost:3000/todos/${params.id}`)
   const todo = await res.json()  
 
